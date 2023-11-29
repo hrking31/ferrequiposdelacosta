26f00c2f29aa24146ba14b731323bb06670888cd
hrking31 <hrking31qgmail.com>
2023-11-29 11:36:07 -0500

    se coloca en gitignore firebaseconfig.js

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -1,17 +1,10 @@
+import firebaseConfig from "./firebaseConfig";
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAxFuwn9lwmYwkxsyD28XBnTu5EDRBIB2g",
-  authDomain: "ferrequiposdelacosta-e2457.firebaseapp.com",
-  projectId: "ferrequiposdelacosta-e2457",
-  storageBucket: "ferrequiposdelacosta-e2457.appspot.com",
-  messagingSenderId: "428909622394",
-  appId: "1:428909622394:web:728f81395dab4fa39e37c3",
-};
-
 const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-export default db;
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
